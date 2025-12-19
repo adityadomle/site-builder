317f074cdc119d5bdc430edbc270b3264ccbf735
Aditya Domle <adityadomle14@gmail.com>
2025-12-19 20:39:20 +0530

    some work on generate img

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,11 +1,130 @@
-import React from 'react'
+import { useState } from "react";
+import { Image, Sparkles } from "lucide-react";
 
 const GenerateImages = () => {
-  return (
-    <div>
-        <h1>GenerateImages</h1>
-    </div>
-  )
-}
+  const imageStyle = [
+    "Realistic",
+    "Ghibli style",
+    "Anime style",
+    "Cartoon style",
+    "Fantasy style",
+    "Realistic style",
+    "3D style",
+    "Portrait style",
+  ];
 
-export default GenerateImages
+  const [selectedStyle, setSelectedStyle] = useState("Realistic");
+  const [input, setInput] = useState("");
+  const [publish, setPublish] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    setTimeout(() => {
+      setContent(
+        "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1080"
+      );
+      setLoading(false);
+    }, 1000);
+  };
+
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* Left col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">AI Image Generator</h1>
+        </div>
+
+        <p className="mt-6 text-sm font-medium">Describe Your Image</p>
+
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          rows={4}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="Describe what you want to see in the image..."
+          required
+        />
+
+        <p className="mt-4 text-sm font-medium">Style</p>
+
+        <div className="mt-3 flex gap-3 flex-wrap">
+          {imageStyle.map((item) => (
+            <span
+              key={item}
+              onClick={() => setSelectedStyle(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
+                selectedStyle === item
+                  ? "bg-green-50 text-green-700"
+                  : "text-gray-500 border-gray-300"
+              }`}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+
+        <div className="my-6 flex items-center gap-2">
+          <label className="relative cursor-pointer">
+            <input
+              type="checkbox"
+              checked={publish}
+              onChange={(e) => setPublish(e.target.checked)}
+              className="sr-only peer"
+            />
+            <div className="w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition"></div>
+            <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4"></span>
+          </label>
+          <p className="text-sm">Make this image Public</p>
+        </div>
+
+        <button
+          disabled={loading}
+          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg"
+        >
+          {loading ? (
+            <span className="w-4 h-4 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <Image className="w-5" />
+          )}
+          Generate Image
+        </button>
+      </form>
+
+      {/* Right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
+        <div className="flex items-center gap-3">
+          <Image className="w-5 h-5 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">Generated image</h1>
+        </div>
+
+        {!content ? (
+          <div className="flex-1 flex justify-center items-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+              <Image className="w-9 h-9" />
+              <p>Enter a topic and click "Generate image"</p>
+            </div>
+          </div>
+        ) : (
+          <div className="mt-3 h-full">
+            <img
+              src={content}
+              alt="generated"
+              className="w-full h-full object-cover rounded-md"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default GenerateImages;
