35319879309aca1f939f6086bd220fb3a5004c64
Aditya Domle <adityadomle14@gmail.com>
2025-12-20 00:58:09 +0530

    icon added

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,6 +1,7 @@
 import { useUser } from "@clerk/clerk-react"
 import React, { useEffect, useState } from "react"
 import { dummyPublishedCreationData } from "../assets/assets"
+import { Heart } from "lucide-react"
 
 const Community = () => {
 
@@ -19,17 +20,35 @@ const Community = () => {
   },[user])
 
   return (
-<div className="flex-1 h-full flex flex-col gap-4 p-6">
-  <h2>Creations</h2>
+    <div className="flex-1 h-full flex flex-col gap-4 p-6">
+      Creations
+      <div className="bg-white h-full w-full rounded-xl overflow-y-scroll">
+        {creations.map((creation, index) => (
+          <div
+            key={index}
+            className="relative group inline-block pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3"
+          >
+            <img
+              src={creation.content}
+              alt=""
+              className="w-full h-full object-cover rounded-lg"
+            />
 
-  <div className="bg-white h-full w-full rounded-xl overflow-y-scroll">
-    {creations.map((item, index) => (
-      <div key={index} className="p-4 border-b">
-        {item}
+
+            <div>
+              <p className="text-sm hidden group-hover:block">{creation,prompt}</p>
+              <div>
+                <p>
+                  {creation.likes,length}
+                </p>
+                <Heart />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-</div>
+    </div>
   )
 }
+
 export default Community
