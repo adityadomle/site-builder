df7f53ffef500ac9d8593c5e83c05fda49e25e2a
Aditya Domle <adityadomle14@gmail.com>
2025-12-24 16:53:40 +0530

    Added GEMINI Api

--- server/controllers/aicontroller.js
+++ server/controllers/aicontroller.js
@@ -1,4 +1,8 @@
-
+import OpenAI from "openai";
+const openai = new OpenAI({
+    apiKey: "GEMINI_API_KEY",
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
 
 export const generateArticle = async (req, res) => {
     try {
